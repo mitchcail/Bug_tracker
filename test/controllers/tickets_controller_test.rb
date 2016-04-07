c0cf6ed16ac47e3d9c0379bedad886d745c47a32
Mamoon <mamoon.alheti@gmail.com>
2016-04-07 01:45:12 -0400

    fixed some issues with testing setup, and added test cases

--- test/controllers/tickets_controller_test.rb
+++ test/controllers/tickets_controller_test.rb
@@ -8,7 +8,34 @@ class TicketsControllerTest < ActionController::TestCase
    def test_ticket
 
 	  	@ticket = Ticket.new
-	  	@ticket.name = "test ticket"
-	    assert_equal("test ticket", ticket.name)
+	  	@ticket.name = "Test ticket"
+	    assert_equal("Test ticket", @ticket.name )
 	end
+
+	def test_comment
+
+	  	@comment = Comment.new
+	  	@comment.text = "this is an intersting bug"
+	    assert_equal("this is an intersting bug", @comment.text )
+
+	end
+
+	def test_user
+
+	  	@user = User.new
+	  	@user.email = "test@testMail.com"
+	    assert_equal("test@testMail.com", @user.email)
+
+	end
+
+	def test_ticket_user
+
+	  	@user = User.new 
+	  	@ticket = @user.tickets.new
+	  	assert_not_nil(@ticket.user)
+
+	end
+
+
+
 end
