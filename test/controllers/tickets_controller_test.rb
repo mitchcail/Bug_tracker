fd74e87a9887532cb040f1f4321855ddc713faf5
Mamoon <mamoon.alheti@gmail.com>
2016-04-07 00:51:43 -0400

    added some features to the profile

--- test/controllers/tickets_controller_test.rb
+++ test/controllers/tickets_controller_test.rb
@@ -4,4 +4,11 @@ class TicketsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+   def test_ticket
+
+	  	@ticket = Ticket.new
+	  	@ticket.name = "test ticket"
+	    assert_equal("test ticket", ticket.name)
+	end
 end
